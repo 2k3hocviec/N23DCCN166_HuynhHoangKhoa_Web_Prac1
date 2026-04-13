1d019400031563218a39ee527e9672c26f70b3c8
2k3hocviec <khoahoian0322003@gmail.com>
2026-04-13 16:01:41 +0700

    Hoàn thiện Blog listing

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
+import BlogCard from "./components/BlogCard";
+
 async function getPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
 }
 
 export default async function HomePafe() {
   const posts = await getPosts();
-  console.log(posts);
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        {posts.slice(0, 12).map((post) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 }
